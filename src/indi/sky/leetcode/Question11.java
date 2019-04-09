91ea22b475641bf4e37a32463ad5c41d75fcb64e
SkyMing <skyming1126@gmail.com>
2019-04-09 21:55:14 +0800

    update Q11

--- src/indi/sky/leetcode/Question11.java
+++ src/indi/sky/leetcode/Question11.java
@@ -16,9 +16,27 @@ public class Question11 {
         }
         return res;
     }
+
+    /**
+     * Description :双指针,向内移动较矮的
+     */
+    public int maxArea0(int[] height){
+        int n = height.length;
+        int res = 0;
+        for (int i = 0, j = n-1; i<j;) {
+            res = Math.max(res,(j-i) * Math.min(height[i],height[j]));
+            if (height[i]<height[j]){
+                i++;
+            }else {
+                j--;
+            }
+        }
+        return res;
+    }
     public static void main(String[] args){
         Question11 q = new Question11();
-        int res = q.maxArea(new int[]{1,8,6,2,5,4,8,3,7});
+//        int res = q.maxArea0(new int[]{1,1});
+        int res = q.maxArea0(new int[]{1,8,6,2,5,4,8,3,7});
         System.out.println("result:"+res);
     }
 }
