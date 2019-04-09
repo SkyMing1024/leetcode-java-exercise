316ba98bb1a1f30282814ed0f196ae8ac107bdfb
SkyMing <skyming1126@gmail.com>
2019-04-09 21:17:46 +0800

    update Q11

--- src/indi/sky/leetcode/Question11.java
+++ src/indi/sky/leetcode/Question11.java
@@ -6,7 +6,19 @@ package indi.sky.leetcode;
 public class Question11 {
 
     public int maxArea(int[] height) {
-
-        return 0;
+        int n = height.length;
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                int temp = (j-i) * Math.min(height[i],height[j]);
+                res = Math.max(res,temp);
+            }
+        }
+        return res;
+    }
+    public static void main(String[] args){
+        Question11 q = new Question11();
+        int res = q.maxArea(new int[]{1,8,6,2,5,4,8,3,7});
+        System.out.println("result:"+res);
     }
 }
