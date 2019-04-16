db20e74a332c4efd39ce3e1ac6df6fcba219d22c
SkyMing <skyming1126@gmail.com>
2019-04-16 23:57:03 +0800

    add TreeNode

---
+++ src/indi/sky/dataStructure/TreeNode.java
@@ -0,0 +1,13 @@
+package indi.sky.dataStructure;
+
+/**
+ */
+public class TreeNode {
+    public Object data;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(Object data) {
+        this.data = data;
+    }
+}
