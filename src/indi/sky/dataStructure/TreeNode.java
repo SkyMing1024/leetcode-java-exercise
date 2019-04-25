90ad507b1a56a4f918b78b3eb0b8036ea52a7b26
SkyMing <skyming1126@gmail.com>
2019-04-25 21:50:52 +0800

    update TreeNode

--- src/indi/sky/dataStructure/TreeNode.java
+++ src/indi/sky/dataStructure/TreeNode.java
@@ -4,10 +4,14 @@ package indi.sky.dataStructure;
  */
 public class TreeNode {
     public Object data;
+    public int val;
     public TreeNode left;
     public TreeNode right;
 
     public TreeNode(Object data) {
         this.data = data;
+    }
+    public TreeNode(int val) {
+        this.val = val;
     }
 }
