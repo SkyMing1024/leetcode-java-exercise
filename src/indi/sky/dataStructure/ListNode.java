a38a46bc965ac8b403dab9acbc20c8cdc46c8e9b
SkyMing <skyming1126@gmail.com>
2019-04-26 23:59:04 +0800

    update ListNode

--- src/indi/sky/dataStructure/ListNode.java
+++ src/indi/sky/dataStructure/ListNode.java
@@ -8,6 +8,26 @@ public class ListNode {
         this.val = val;
     }
 
+    /**
+     * Description : 删除第n个元素 n=1 . 2 . 3......
+     */
+    public void remov(int index){
+        ListNode res = this;
+        int count = 0;
+        while (res!=null){
+            count++;
+            if (count == index-1){
+                res.next = res.next.next;
+                break;
+            }else {
+                res = res.next;
+            }
+        }
+    }
+
+    /**
+     * Description : 打印链表
+     */
     public void print(){
         ListNode tmp = this;
         StringBuilder res = new StringBuilder();
