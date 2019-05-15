c4da0776b919f3618e691402b7cf51386311beaf
skyming <skyming1126@gmail.com>
2019-05-15 23:20:17 +0800

    add q23

---
+++ src/indi/sky/leetcode/Question23.java
@@ -0,0 +1,13 @@
+package indi.sky.leetcode;
+
+import indi.sky.dataStructure.ListNode;
+
+/**
+ * 23 合并K个排序链表
+ */
+public class Question23 {
+    public ListNode mergeKLists(ListNode[] lists) {
+
+        return lists[0];
+    }
+}
