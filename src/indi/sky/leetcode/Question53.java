32e0dc8a6c2933c44fb4717128cbe06cd97ce114
skyming <skyming1126@gmail.com>
2019-05-27 23:27:24 +0800

    update 53

--- src/indi/sky/leetcode/Question53.java
+++ src/indi/sky/leetcode/Question53.java
@@ -11,19 +11,17 @@ public class Question53 {
      * dp[i] = max(dp[i-1]+sums[i],sums[i])
      */
     public int maxSubArray31(int[] nums) {
-        if (nums.length == 0) return 0;
-        if (nums.length == 1) return nums[0];
+        if(nums.length == 0)return 0;
         int[] dp = new int[nums.length];
         dp[0] = nums[0];
-        int index = 0;
-        for (int i = 1; i < nums.length; i++) {
-            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+        for(int i = 1; i< nums.length;i++){
+            dp[i] = Math.max(dp[i-1]+nums[i],nums[i]);
         }
-        int max = 0;
-        for (int i = 0; i < dp.length; i++) {
-            max = Math.max(max, dp[i]);
+        int res = dp[0];
+        for(int i = 0; i< dp.length;i++){
+            res = Math.max(dp[i],res);
         }
-        return max;
+        return res;
     }
 
     /**
