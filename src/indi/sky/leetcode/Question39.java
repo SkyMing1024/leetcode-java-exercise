06851c451acc91ec48d178c835965f68924023e1
skyming <skyming1126@gmail.com>
2019-06-02 23:59:06 +0800

    update q39

---
+++ src/indi/sky/leetcode/Question39.java
@@ -0,0 +1,14 @@
+package indi.sky.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 39. 组合总和
+ */
+public class Question39 {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        return list;
+    }
+}
