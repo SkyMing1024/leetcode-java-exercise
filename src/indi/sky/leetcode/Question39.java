4d901f3b0b584f73cb77a5ea2816aaeb6b3a5633
skyming <skyming1126@gmail.com>
2019-06-03 23:56:52 +0800

    update q39

--- src/indi/sky/leetcode/Question39.java
+++ src/indi/sky/leetcode/Question39.java
@@ -1,6 +1,7 @@
 package indi.sky.leetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -8,7 +9,23 @@ import java.util.List;
  */
 public class Question39 {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> list = new ArrayList<>();
-        return list;
+        Arrays.sort(candidates);
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(candidates, target, res, 0, new ArrayList<Integer>());
+        return res;
+    }
+
+    private void backtrack(int[] candidates, int target, List<List<Integer>> res, int start, List<Integer> list) {
+        if (target < 0) return;
+        if (target == 0) {
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = start; i < candidates.length; i++) {
+            if (target < candidates[i]) break;
+            list.add(candidates[i]);
+            backtrack(candidates, target - candidates[i], res, i, list);
+            list.remove(list.size() - 1);
+        }
     }
 }
